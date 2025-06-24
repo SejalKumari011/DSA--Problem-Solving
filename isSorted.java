4da09f7cb1b5ca43936c3862680c37ec5eeef894
[DELL] <[sejalsavran2004@gmail.com]>
2025-06-24 07:59:18 -0700

    Added problem array is sorted or not

---
+++ isSorted.java
@@ -0,0 +1,22 @@
+public class isSorted {
+
+    public static boolean IsSorted(int arr[],int i){
+            if(i==arr.length-1){
+                return true;
+            }
+
+            if(arr[i]>arr[i+1]){
+                return false;
+            }
+
+            return IsSorted(arr,i+1);         
+    }
+
+    public static void main(String [] args){
+
+        int arr[]={1,2,10,4,5};
+        System.out.println(IsSorted(arr, 0));
+
+    }
+    
+}
