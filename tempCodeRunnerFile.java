f63ebe9c4974610c0ea24d9ae3a2f843b1f2ed7e
[DELL] <[sejalsavran2004@gmail.com]>
2025-07-24 23:10:50 -0700

    added first non repeating letter

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,17 +1,1 @@
-public class SumOfN {
-
-    public static int sumOfNNatural(int n){
-        if(n==1){
-            return 1;
-        }
-
-        int fnm1=sumOfNNatural(n-1);
-        return n+fnm1;
-    }
-
-    public static void main(String [] args){
-        int n=5;
-        System.out.println(sumOfNNatural(n));
-    }
-    
-}
+str
